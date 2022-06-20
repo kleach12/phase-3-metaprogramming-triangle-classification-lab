c63dea23cbb0f8a59c6b4f9651e9ade68993675c
Kale Leach <kleach1212@gmail.com>
2022-06-20 14:24:36 -0700

    commit tree f953437c8069b26382918dbdc163298781ef7ed5

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,40 @@
 class Triangle
-  # write code here
+  
+  attr_accessor :side_1, :side_2, :side_3, :type
+
+  def initialize(side_1, side_2, side_3)
+  @side_1 = side_1
+  @side_2 = side_2
+  @side_3 = side_3
+  
+  end
+
+  def kind
+    if  all_have_one && all_equal_two
+      
+      if self.side_1 == self.side_2 && self.side_1 == self.side_3
+        @type = :equilateral
+      elsif self.side_1 == self.side_2 || self.side_1 == side_3 || self.side_2 == self.side_3
+        @type = :isosceles
+      else
+        @type = :scalene
+      end
+
+      else 
+        raise TriangleError
+    end
+  end
+
+  def all_have_one
+    self.side_1 > 0 && self.side_2 > 0 && self.side_3 > 0
+  end
+
+  def all_equal_two
+    self.side_1 + self.side_2 > self.side_3 && self.side_1 + self.side_3 > self.side_2 && self.side_3 + self.side_2 > self.side_1 
+  end
+
+  class TriangleError < StandardError
+
+  end
+
 end
